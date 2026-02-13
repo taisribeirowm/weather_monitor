de02b6d05d4e63c3f7c8e16d1eaf01c48e06af5c
Tais.Ribeiro <thais.ribeiro@WMM01949.local>
2026-02-13 04:01:57 -0300

    Start project

---
+++ WeatherMonitor/WeatherMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherMonitor
+//
+//  Created by Tais.Ribeiro on 13/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherMonitor/WeatherMonitor/WeatherMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMonitorApp.swift
+//  WeatherMonitor
+//
+//  Created by Tais.Ribeiro on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
